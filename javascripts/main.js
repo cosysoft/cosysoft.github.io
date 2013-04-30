702aee68649c30f505ea823201f6f79e263f8648
remotejavaksy <remotejavasky@163.com>
2013-04-30 12:27:15 +0800

    init

---
+++ javascripts/main.js
@@ -0,0 +1,8 @@
+console.log('This would be the main JS file.');
+
+if (navigator.language == "zh-CN") {
+	location = "index.zh_CN.html";
+}
+if (navigator.userLanguage == "zh-CN") {
+	location = "index.zh_CN.html";
+}
